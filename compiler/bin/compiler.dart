6a1ce3e4556a2fe1f9672f3f41b522e7c3e0563e
lo5 <prithvi.prabhu@gmail.com>
2021-10-22 09:35:31 -0700

    wip: Fix seed project path

--- compiler/bin/compiler.dart
+++ compiler/bin/compiler.dart
@@ -1,5 +1,5 @@
 import 'package:compiler/compiler.dart' as compiler;
 
 void main(List<String> arguments) {
-  compiler.compile('../loader/lib/main.dart', '../python/package/h2o_nitro');
+  compiler.compile('../client/lib/main.dart', '../python/package/h2o_nitro');
 }
