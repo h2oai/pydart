4648d724c7054d93edf613c768721c33d8c222b7
lo5 <prithvi.prabhu@gmail.com>
2021-11-18 08:00:11 -0800

    refactor: Move loading to load.dart

--- compiler/lib/compiler.dart
+++ compiler/lib/compiler.dart
@@ -1,17 +1,11 @@
 import 'dart:io';
 
-import 'package:analyzer/dart/analysis/analysis_context_collection.dart';
-import 'package:analyzer/dart/analysis/results.dart';
 import 'package:analyzer/dart/element/element.dart';
 import 'package:path/path.dart' as path;
 
 import 'ir.dart';
+import 'load.dart';
 import 'python.dart';
-
-void compile(String loaderPath, String outputDir) {
-  _analyze(path.normalize(File(loaderPath).absolute.path))
-      .then((es) => _postProcess(es, outputDir));
-}
 
 const libraryWhitelist = {
   'material',
@@ -30,6 +24,14 @@ const widgetWhitelist = {
 
 const libNames = <String>[];
 
+void compile(String loaderPath, String outputDir) {
+  load(
+          sourcePath: path.normalize(File(loaderPath).absolute.path),
+          libraryWhitelist: libraryWhitelist,
+          widgetWhitelist: widgetWhitelist)
+      .then((es) => _postProcess(es, outputDir));
+}
+
 void _postProcess(Set<Element> elements, String outputDir) {
   _mkdirp(outputDir);
 
@@ -40,73 +42,8 @@ void _postProcess(Set<Element> elements, String outputDir) {
   _write(path.join(outputDir, 'types.py'), lines);
 }
 
-Future<Set<Element>> _analyze(String sourcePath) async {
-  // Important: The analyzer resolves symbols correctly only if the file
-  // .dart_tool/package_config.json is found inside the source project.
-
-  // https://groups.google.com/a/dartlang.org/g/analyzer-discuss/c/Z8hIZDYvUxk
-  // -----
-  // When the analyzer needs to resolve a URI to a file location there are three
-  // options:
-  // - If the URI is a `dart:` URI, then the library is found in a known
-  //   location in the sdk.
-  // - If the URI is a `package:` URI, then the analyzer looks in each of the
-  //   parent directories containing the library for a file whose relative path
-  //   is `.dart_tool/package_config.json`. That file contains a JSON encoding
-  //   of a map from the name of the package (`flutter` in this case) to the
-  //   path of the `lib` directory for that package
-  //   (`<path>/flutter/packages/flutter/lib` in this case).
-  //   Everything after the first `/` in the URI is then appended to the path
-  //   to form the full path to the file.
-  // - If the URI is any other kind of URI, then the normal URI resolution
-  //   rules are followed.
-  //
-  // The most likely problem here is that the analyzer is unable to locate the
-  // `package_config.json` file and hence can't convert the URI into a file path.
-  //
-  // By the way, the process of finding the `package_config.json` file happens
-  // at the time you create the `AnalysisContextCollection` so that it doesn't
-  // need to be repeated for each library you analyze. That, and the locations
-  // of the `analysis_options.yaml` files, is what determines how many analysis
-  // contexts get created in the collection.
-  // ---
-
-  final analysis = AnalysisContextCollection(includedPaths: [sourcePath]);
-  final elements = <Element>{};
-  for (final ctx in analysis.contexts) {
-    for (final filePath in ctx.contextRoot.analyzedFiles()) {
-      final result = await ctx.currentSession.getResolvedLibrary(filePath);
-      if (result is ResolvedLibraryResult) {
-        final lib = result.element;
-        for (final lib in lib.importedLibraries) {
-          if (!libraryWhitelist.contains(lib.name)) continue;
-          elements.addAll(lib.exportNamespace.definedNames.values);
-          elements.addAll(_getDefinedElements(lib.definingCompilationUnit));
-          for (final part in lib.parts) {
-            elements.addAll(_getDefinedElements(part));
-          }
-        }
-      }
-    }
-  }
-  return elements;
-}
-
 void _write(String path, String text) => File(path).writeAsStringSync(text);
 
 String _read(String path) => File(path).readAsStringSync();
 
 void _mkdirp(String path) => Directory(path).createSync(recursive: true);
-
-Iterable<Element> _getDefinedElements(CompilationUnitElement e) {
-  return [
-    e.accessors,
-    e.classes,
-    e.enums,
-    e.extensions,
-    e.functions,
-    e.mixins,
-    e.topLevelVariables,
-    e.typeAliases,
-  ].expand((e) => e);
-}
---
+++ compiler/lib/load.dart
@@ -0,0 +1,71 @@
+import 'package:analyzer/dart/analysis/analysis_context_collection.dart';
+import 'package:analyzer/dart/analysis/results.dart';
+import 'package:analyzer/dart/element/element.dart';
+
+Future<Set<Element>> load(
+    {required String sourcePath,
+    required Set<String> libraryWhitelist,
+    required Set<String> widgetWhitelist}) async {
+  // Important: The analyzer resolves symbols correctly only if the file
+  // .dart_tool/package_config.json is found inside the source project.
+
+  // https://groups.google.com/a/dartlang.org/g/analyzer-discuss/c/Z8hIZDYvUxk
+  // -----
+  // When the analyzer needs to resolve a URI to a file location there are three
+  // options:
+  // - If the URI is a `dart:` URI, then the library is found in a known
+  //   location in the sdk.
+  // - If the URI is a `package:` URI, then the analyzer looks in each of the
+  //   parent directories containing the library for a file whose relative path
+  //   is `.dart_tool/package_config.json`. That file contains a JSON encoding
+  //   of a map from the name of the package (`flutter` in this case) to the
+  //   path of the `lib` directory for that package
+  //   (`<path>/flutter/packages/flutter/lib` in this case).
+  //   Everything after the first `/` in the URI is then appended to the path
+  //   to form the full path to the file.
+  // - If the URI is any other kind of URI, then the normal URI resolution
+  //   rules are followed.
+  //
+  // The most likely problem here is that the analyzer is unable to locate the
+  // `package_config.json` file and hence can't convert the URI into a file path.
+  //
+  // By the way, the process of finding the `package_config.json` file happens
+  // at the time you create the `AnalysisContextCollection` so that it doesn't
+  // need to be repeated for each library you analyze. That, and the locations
+  // of the `analysis_options.yaml` files, is what determines how many analysis
+  // contexts get created in the collection.
+  // ---
+
+  final analysis = AnalysisContextCollection(includedPaths: [sourcePath]);
+  final elements = <Element>{};
+  for (final ctx in analysis.contexts) {
+    for (final filePath in ctx.contextRoot.analyzedFiles()) {
+      final result = await ctx.currentSession.getResolvedLibrary(filePath);
+      if (result is ResolvedLibraryResult) {
+        final lib = result.element;
+        for (final lib in lib.importedLibraries) {
+          if (!libraryWhitelist.contains(lib.name)) continue;
+          elements.addAll(lib.exportNamespace.definedNames.values);
+          elements.addAll(_getDefinedElements(lib.definingCompilationUnit));
+          for (final part in lib.parts) {
+            elements.addAll(_getDefinedElements(part));
+          }
+        }
+      }
+    }
+  }
+  return elements;
+}
+
+Iterable<Element> _getDefinedElements(CompilationUnitElement e) {
+  return [
+    e.accessors,
+    e.classes,
+    e.enums,
+    e.extensions,
+    e.functions,
+    e.mixins,
+    e.topLevelVariables,
+    e.typeAliases,
+  ].expand((e) => e);
+}
