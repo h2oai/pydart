5fa92c9d0e2a75b369a636e8b04229b04434057a
lo5 <prithvi.prabhu@gmail.com>
2021-11-19 11:43:21 -0800

    fix: Handle collections while dumping widget

--- python/h2o_nitro/core.py
+++ python/h2o_nitro/core.py
@@ -2,12 +2,15 @@ import json
 from typing import Any
 
 
-def _unwrap_rec(x: dict) -> dict:
-    return {k: _unwrap_rec(v._nx_) if hasattr(v, '_nx_') else v for k, v in x.items() if v is not None}
-
-
-def _unwrap(x: Any) -> dict:
-    return _unwrap_rec(x._nx_) if hasattr(x, '_nx_') else x
+def _dump(xs: Any):
+    if isinstance(xs, (list, tuple)):
+        return [_dump(x) for x in xs]
+    elif isinstance(xs, dict):
+        return {k: _dump(v) for k, v in xs.items()}
+    elif hasattr(xs, '_nx_'):
+        return _dump(xs._nx_)
+    else:
+        return xs
 
 
 def _marshal(x: Any) -> str:
@@ -24,7 +27,7 @@ class UI:
         self._changes = []
 
     def __setitem__(self, key: str, value: Any):
-        self._changes.append((key, _unwrap(value)))
+        self._changes.append((key, _dump(value)))
 
     def __delitem__(self, key: str):
         self._changes.append((key,))
